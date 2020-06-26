508ee05875e03033f61ccc4eaa59f0d97cf18314
yanshifu <yanshifu2012@hotmail.com>
2020-06-26 10:35:30 +0900

    aa

---
+++ AspNetFrameWebApp/MsTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+using AspNetFrameWebApp.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MsTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Calculator calculator = new Calculator();
+            var result = calculator.Sum(4, 5, 6);
+            Assert.AreEqual<int>(15, result);
+        }
+    }
+}
